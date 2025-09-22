b8ee2f68f759ae1cb1052a6f93dee341c20c35da
Jesus Mendoza <albertomendozax9800@gmail.com>
2025-09-22 13:31:30 -0600

    22/09/2025

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -92,7 +92,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.config(default="sqlite:///db.sqlite3")
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
 }
 
 
